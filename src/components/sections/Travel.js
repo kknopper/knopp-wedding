bb13f67bd44f48abdfbd5f1f5abbfe44c28ce4e6
Kevin Knopp <kknopp@apple.com>
2021-09-20 12:51:31 -0700

    local working

--- src/components/sections/Travel.js
+++ src/components/sections/Travel.js
@@ -1,17 +1,13 @@
 import * as React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import { faRoute } from "@fortawesome/free-solid-svg-icons";
 import Section from '../section';
 
-const StyledSection = styled(Section)`
-	grid-area: travel;
-`
-
 const Travel = () => {
 	return (
-		<StyledSection id="travel" headlineIcon={faRoute}>
+		<Section id="travel" headlineIcon={faRoute}>
 			<h3>Flying in?</h3>
-		</StyledSection>
+		</Section>
 	)
 }
 
