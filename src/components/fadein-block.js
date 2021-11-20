5581715f075a61a24b26f73d89a83882ac993891
Kevin Knopp <kknopp@apple.com>
2021-11-20 15:31:05 -0800

    train whistle fix

--- src/components/fadein-block.js
+++ src/components/fadein-block.js
@@ -2,49 +2,25 @@ import * as React from 'react';
 import styled from 'styled-components';
 import { useSpring, animated } from 'react-spring';
 import { useInView, InView } from "react-intersection-observer";
+import useSound from 'use-sound';
 import trainWhistle from '../audio/train-whistle.m4a';
 
 const Fade = styled(animated.div)`
 	display: block;
 `
 
-const AudioTag = styled.audio`
-	visibility: hidden;
-`;
-
 const FadeIn = ({playAudio, children}) => {
 	const [ref, inView] = useInView({
 		triggerOnce: true,
 		rootMargin: '-200px 0px',
 	});
-
-	const [audioFile] = React.useState(new Audio(trainWhistle));
-	// console.log(audioFile)
-	const [playing, setPlaying] = React.useState(false);
-
-	const toggleAudio = () => setPlaying(!playing);
-
-	React.useEffect(() => {
-		playing ? audioFile.play() : audioFile.pause();
-		console.log('playing', playing);
-	},
-		[playing]
-	);
-
-	React.useEffect(() => {
-		audioFile.addEventListener('ended', () => setPlaying(false));
-		return () => {
-			audioFile.removeEventListener('ended', () => setPlaying(false));
-		};
-	}, []);
-
-
+	const [play] = useSound(trainWhistle);
 	const animation = useSpring({ to: { opacity: inView ? 1 : 0, y: inView ? 0 : -30}, from: { opacity: 0, y: -30 }, config: { duration: 400 }, });
 
 	return (
 		<Fade ref={ref} style={animation}>
 			{playAudio ? 
-				<InView triggerOnce="true" onChange={(inView, entry) => {if (inView) toggleAudio()}} />
+				<InView triggerOnce="true" onChange={(inView, entry) => {if (inView) play()}} />
 			: null}
 			{children}
 		</Fade>
